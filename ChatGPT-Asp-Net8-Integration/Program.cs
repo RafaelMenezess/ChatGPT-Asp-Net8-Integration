4e06ab8dbf7d3f70befa272efe68ab774054063d
Rafael Menezes <rafa.rcm@hotmail.com>
2024-08-12 10:34:34 -0300

    Criando a Classe SwaggerExtensions para Consumir a API do ChatGPT

--- ChatGPT-Asp-Net8-Integration/Program.cs
+++ ChatGPT-Asp-Net8-Integration/Program.cs
@@ -1,13 +1,16 @@
 using ChatGPT_Asp_Net8_Integration.Extensions;
 
+var appName = "ChatGPT Asp.Net 8 Integration";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddChatGpt(/*builder.Configuration*/);
-builder.AddSerilog(builder.Configuration, "ChatGPT Asp.Net 8 Integration");
+builder.AddSerilog(builder.Configuration, appName);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwagger(builder.Configuration, appName);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,11 +18,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerDoc(appName);
 
 app.UseHttpsRedirection();
 
